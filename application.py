f27135f0dc7af1fc61a61846859835e5a670d63f
Akash Sharma <akashsharma123.1995@gmail.com>
2018-09-17 10:24:53 +0530

    resources

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello, World!"
+
+@app.route("/<string:name>")
+def hello(name):
+    name = name.capitalize()
+    return f"<h1>hello, {name}</h1>"
+
+
+    
---
+++ classes.py
@@ -0,0 +1,10 @@
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+a = Point(3,5)
+b = Point(4,5)
+
+print(a.x)
+print(a.y)
